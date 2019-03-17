1728be1ee15cf24b21a98cfaa8cf2bb374fcf650
Bruno Adami <brunoadamiserine@gmail.com>
2019-03-16 23:21:31 -0300

    Updates

---
+++ precache-manifest.42445b107acd689b2e11ed28dc5c4d54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b10db3aa48cafdcebc6e",
+    "url": "/Face-recognition-game/static/css/main.4fbbc9eb.chunk.css"
+  },
+  {
+    "revision": "b10db3aa48cafdcebc6e",
+    "url": "/Face-recognition-game/static/js/main.f936de48.chunk.js"
+  },
+  {
+    "revision": "fa18866944d4fca87f46",
+    "url": "/Face-recognition-game/static/js/runtime~main.24b4a681.js"
+  },
+  {
+    "revision": "4ff974dbffdecc265070",
+    "url": "/Face-recognition-game/static/css/2.b5e45ae6.chunk.css"
+  },
+  {
+    "revision": "4ff974dbffdecc265070",
+    "url": "/Face-recognition-game/static/js/2.c265e8e6.chunk.js"
+  },
+  {
+    "revision": "ab41af34b531635d8831a3cec59d90ed",
+    "url": "/Face-recognition-game/static/media/face.ab41af34.png"
+  },
+  {
+    "revision": "40986ff91b19c21a9268367e2c06a3dd",
+    "url": "/Face-recognition-game/index.html"
+  }
+];
